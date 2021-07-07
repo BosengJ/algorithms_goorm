c155a956a9d5c9f004ab84c1d0ee08f7e9f3f1d5
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-07-07 17:50:45 +0900

    Update 02_shift.py

--- 02_shift.py
+++ 02_shift.py
@@ -1,7 +1,20 @@
-a = 10
-b = -10
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+# user_input = input()
+# print ("Hello Goorm! Your input is " + user_input)
 
-c = bin(a)
-d = bin(b)
+a,b = map(int,input().split())
 
-print(c,d)
+bin_a = bin(a)[2:]
+
+#Right shift
+if a < 0:
+	r_shift = ('1' * b) + bin_a[:-b]
+else:
+	r_shift = ('0' * b) + bin_a[:-b]
+	
+#Left shift
+l_shift = bin_a[b:] + ('0' * b)
+
+print(r_shift)
+print(l_shift)
