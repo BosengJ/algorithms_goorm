0fd27f1e7f94c37ce38cf2f90f30f9da5fbd447f
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-07-11 19:53:23 +0900

    Update 07_palindrome.py

--- 07_palindrome.py
+++ 07_palindrome.py
@@ -9,13 +9,29 @@ n = int(input())
 word_list = []
 for i in range(n):
 	word_list.append(input())
+	
+# def pseudoPD(word):
+# 	for i in range(len(word)):
+# 		del_word = word[:i] + word[(i+1):]
+# 		reversed_del_word = del_word[::-1]
+# 		if del_word == reversed_del_word:
+# 			return True
+# 	return False
+
+def checkString(s):
+	for i in range(len(s)//2):
+		if s[i] != s[-i-1]:
+			return False, i
+	return True, i
 
 def pseudoPD(word):
-	for i in range(len(word)):
-		del_word = word[:i] + word[(i+1):]
-		reversed_del_word = del_word[::-1]
-		if del_word == reversed_del_word:
-			return True
+	TF, idx = checkString(word)
+	s1 = word[:idx] + word[idx+1:]
+	s2 = word[:len(word)-idx-1] + word[len(word)-idx:]
+	TF_s1, _ = checkString(s1)
+	TF_s2, _ = checkString(s2)
+	if (TF_s1 == True) or (TF_s2 == True):
+		return True
 	return False
 
 for word in word_list:
